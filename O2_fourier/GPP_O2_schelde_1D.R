43569c1bab5cb9f25d2f493f93b4c768e106b186
Willem Stolte <willem.stolte@deltares.nl>
2024-12-18 18:30:40 +0100

    aangepast op manuscript

--- O2_fourier/GPP_O2_schelde_1D.R
+++ O2_fourier/GPP_O2_schelde_1D.R
@@ -24,12 +24,12 @@ stationlist = c(
   '35' = 'Terneuzen'
 )  
 
- modeltimedata = tibble(
+modeltimedata = tibble(
   time = 
     seq(ymd_hms('2014-03-03 00:00:00'), ymd_hms('2014-05-03 00:00:00'), by='10 min')
 )
 
-stations = names(stationlist)
+ stations = names(stationlist)
 
 modeloutputlist <- lapply(
   stations,
@@ -62,14 +62,14 @@ ggp_fourier <- list_ggpInput_df %>%
   map(
     function(df) WindowGPPFourier(
       x = as.data.frame(df),
-      # Nfilt = 1,
       phi = 51.176932, 
       lambda = 4.325788,
-      Width = 1#,
-      # filtWidth = 10
+      Width = 1
     )
   ) %>%
-  bind_rows(.id = "station")
+  bind_rows(.id = "station") %>%
+  rename(datetime = time) %>%
+  select(-t1, -t2)
 
 
 # calculate volumetric gpp with fourier method with lowpass filter for tides
@@ -84,20 +84,21 @@ ggp_fourier_t <- list_ggpInput_df %>%
     )
   ) %>%
   bind_rows(.id = "station") %>%
-  rename(
-    datetime = times,
-  )
+  group_by(station, date = as_date(times)) %>%
+  summarize(GPPt = mean(GPPt, na.rm = T), .groups = "drop") %>%
+  mutate(datetime = as_datetime(paste(date, "12:00:00")))
+  
 
 # check diff between gpp and gpp_t
 
 ggplot() +
   geom_point(
     data = ggp_fourier,
-    aes(time, GPP, color = "gpp")
+    aes(datetime, GPP, color = "gpp")
     ) +
   geom_line(
     data = ggp_fourier_t,
-    aes(times, GPPt, color = "gppt")
+    aes(datetime, GPPt, color = "gppt")
   ) +
   facet_wrap(
     "station", 
@@ -112,6 +113,17 @@ modeloutput <- modeloutputlist %>%
         datetime = time
       )
 
+modeloutput_day_avg <- modeloutput %>%
+  group_by(station, date = as_date(datetime)) %>%
+  summarize(
+    o2avg_ds_day_avg = mean(o2avg_ds),
+    o2L2_ds_day_avg = mean(o2L2_ds),
+    Depth_ds_day_avg = mean(Depth_ds),
+  ) %>%
+  mutate(datetime = as_datetime(paste(date, "12:00:00"))) %>%
+  select(-date)
+
+# gpp from model is alrady dat averaged and is expressed as grams C per m2
 GPP_from_model <- lapply(
   stations, function(station){
     GPPtimeseriespath <- file.path(modeldatadir, paste0("GPP_", station, "ds_day_avg.dat"))
@@ -120,85 +132,61 @@ GPP_from_model <- lapply(
   }
 ) %>% set_names(stationlist) %>%
   bind_rows(.id = "station") %>%
-  rename(
-    datetime = date
-  ) %>%
-  select(
-    -time
-  )
+  rename(datetime = date) %>%
+  select(-time) %>%
+  left_join(modeloutput_day_avg) %>%
+  mutate(GPP_dayavg_ds_m3 = GPP_day_avg_ds_m2/Depth_ds_day_avg)
 
 #=== Grafiek voor publicatie ===================================================
 
-ggp_fourier_t %>% str() # 10-minutes 
-
-GPP_from_model %>% str() # day average
-
-GPP_all <- ggp_fourier_t %>%
-  mutate(fourier_GPPt_C = GPPt/2.67) %>%
-  # group_by(day(datetime)) %>% 
-  # mutate(fourier_GPPt_day_avg_C = mean(fourier_GPPt_d_C)) %>% ungroup() %>%
-  full_join(
-    GPP_from_model
-  ) %>%
-  full_join(
-    modeloutput
-  ) %>%
-  mutate(
-    GPP_day_avg_ds_m3 = GPP_day_avg_ds_m2 / Depth_ds
-  ) %>%
-  # mutate(
-  #   diff_GPP_day_avg = model_GPP_day_avg_ds - fourier_GPPt_d_C
-  # ) %>%
-  select(
-    station,
-    datetime,
-    Depth_ds,
-    fourier_GPPt_C,
-    # fourier_GPPt_day_avg_C,
-    GPP_day_avg_ds_m3
-  ) #%>%
-  # pivot_longer(
-  #   c(
-  #     fourier_GPPt_d_C, 
-  #     # fourier_GPPt_day_avg_C,
-  #     model_GPP_day_avg_ds, 
-  #     diff_GPP_day_avg
-  #   ), 
-  #   names_to = "method", 
-  #   values_to = "GPP in gC/m2/d"
-  # )
-
 # colors from colorbrew set2 for 5 discrete values (https://colorbrewer2.org/?type=qualitative&scheme=Set2&n=5)
 # not used at the moment
-colors = c('#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854')
+plotcolors = c('#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854')
 
-GPP_all %>%
-  # filter(!is.na(`GPP in gC/m2/d` & !is.nan(`GPP in gC/m2/d`))) %>%
-  # distinct() %>%
-  ggplot(
-    aes(x = datetime)) +
-  geom_line(aes(y = fourier_GPPt_C, color = "gpp_fourier"))+
-  geom_point(aes(y = GPP_day_avg_ds_m3, color = "gpp_model")) +
+# Figuur met standaardmethode (ziet er niet zo heel goed uit)
+ggplot() +
+  geom_path(
+    data = GPP_from_model, 
+    aes(datetime, GPP_dayavg_ds_m3, color = "gpp_model")) +
+  geom_point(
+    data = ggp_fourier,
+    aes(datetime, GPP/2.67, color = "gpp_fourier")
+  ) +
+facet_wrap("station", scales = "free", ncol = 1) +
+  ylab("GPP in gC/m3/d") +
+  scale_color_manual(
+    values = c("gpp_fourier" = plotcolors[1],
+               "gpp_model" = plotcolors[2],
+               "difference" = plotcolors[4])
+  ) +
+  ggtitle("gpp fourier vs modelled gpp in gC/m2/d")
+
+# figuur met lopend gemiddelde van 1 dag. Ziet er beter uit. 
+ggplot() +
+  geom_path(
+    data = GPP_from_model, 
+    aes(datetime, GPP_dayavg_ds_m3, color = "gpp_model"),
+    linewidth = 1) +
+  geom_point(
+    data = ggp_fourier_t,
+    aes(datetime, GPPt/2.67, color = "gpp_fourier_t"),
+    method = "loess", span = 0.1
+  ) +
+  geom_segment(
+    data = GPP_from_model %>%
+      left_join(ggp_fourier_t) %>%
+      mutate(diff = GPPt/2.67 - GPP_dayavg_ds_m3),
+    aes(datetime, y = 0, yend = diff, color = "difference"),
+    linewidth = 2
+  ) +
   facet_wrap("station", scales = "free", ncol = 1) +
-  ylab("GPP in gC/m3/d")
-  
-GPP_all %>%
-  group_by(station, day = day(datetime - 60*60*12)) %>% # average over -12 h to +12h
-  summarize(
-    Depth_day_avg = mean(Depth_ds, na.rm = T),
-    fourier_GPPt_C_day_avg = mean(fourier_GPPt_C, na.rm = T),
-    GPP_day_avg_ds_m3 = mean(GPP_day_avg_ds_m3, na.rm = T)
-    ) %>% ungroup() %>%
-  mutate(difference = fourier_GPPt_C_day_avg - GPP_day_avg_ds_m3) %>%
-  ggplot(aes(x = day)) +
-  geom_line(aes(y = fourier_GPPt_C_day_avg, color = "gpp_fourier_day_avg"))+
-  geom_point(aes(y = GPP_day_avg_ds_m3, color = "gpp_model")) +
-  geom_segment(aes(y = 0, yend = difference, color = "difference"), size = 3) +
-  facet_wrap("station", scales = "free", ncol = 1) +
-  ylab("GPP in gC/m3/d")
+  ylab("GPP in gC/m3/d") +
+  scale_color_manual(
+    values = c("gpp_fourier_t" = plotcolors[1],
+               "gpp_model" = plotcolors[2],
+               "difference" = plotcolors[4])
+  ) +
+  ggtitle("gpp fourier vs modelled gpp in gC/m2/d")
 
-
-
-
-
+#===================== older scripts for difference =======================
 
